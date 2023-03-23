79a2cb6126173ca951dfccefad656dcbf81b73f0
Itsyadavishal <vishalyadav17147714@gmail.com>
2023-03-23 17:22:20 +0530

    X2: xperience bringup

--- xperience_X2.mk
+++ xperience_X2.mk
@@ -1,27 +1,34 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The XPerience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from X2 device
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X2
-PRODUCT_DEVICE := X2
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := realme X2
-PRODUCT_MANUFACTURER := realme
+# Inherit some common XPerience stuff
+$(call inherit-product, vendor/xperience/config/common.mk)
 
+PRODUCT_BRAND := realme
+PRODUCT_DEVICE := X2
+PRODUCT_MANUFACTURER := realme
+PRODUCT_NAME := xperience_X2
+PRODUCT_MODEL := realme X2
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1592194586:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Rom Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+#gapps
+WITH_GMS := true
